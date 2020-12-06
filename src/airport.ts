efa3101e7cd4b64c31bcf7fd5093723421757bc6
Shaundai Person <shaundaiperson@gmail.com>
2020-12-05 22:44:33 -0500

    add Passenger type

--- src/airport.ts
+++ src/airport.ts
@@ -1,1 +1,16 @@
-type 
+type Passenger = {
+    firstName: string,
+    lastName: string,
+    gender?: string,
+    race?: string,
+    covid: boolean,
+    ticket: number,
+}
+
+const issueTicket = () => {
+    return
+}
+
+const boardPlane = () => {
+    return
+}
